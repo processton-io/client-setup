dee9f9515ba755991e2405371e473a718d853f4f
Ahmad Faryab Kokab <68786248+afaryab@users.noreply.github.com>
2025-06-01 19:59:22 +0000

    spell mistake

--- packages/processton/customer/database/migrations/2025_04_10_212513_create_customer_contacts_table.php
+++ packages/processton/customer/database/migrations/2025_04_10_212513_create_customer_contacts_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('customer_contacts', function (Blueprint $table) {
             $table->id();
-            $table->foriegnUuid('customer_id')->constrained('customers')->onDelete('cascade');
-            $table->foriegnUuid('contact_id')->constrained('contacts')->onDelete('cascade');
+            $table->foreignUuid('customer_id')->constrained('customers')->onDelete('cascade');
+            $table->foreignUuid('contact_id')->constrained('contacts')->onDelete('cascade');
             $table->string('job_title')->nullable();       // e.g., Manager, Director
             $table->string('department')->nullable();        // Sales, Marketing, etc.
             $table->timestamps();
