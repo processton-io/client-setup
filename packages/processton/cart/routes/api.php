08fa6841a32915239053cf945a19a5e00409de72
Ahmad Faryab Kokab <68786248+afaryab@users.noreply.github.com>
2025-06-02 20:20:54 +0000

    A blunder fixed

--- packages/processton/cart/routes/api.php
+++ packages/processton/cart/routes/api.php
@@ -7,17 +7,17 @@ Route::middleware([
     'api'
 ])->prefix('api')->group(function () {
     Route::prefix('cart')->group(function () {
-        Route::get('/init', [ApiController::class => 'initialize'])->name('processton-cart.api.initialize');
+        Route::get('/init', [ApiController::class , 'initialize'])->name('processton-cart.api.initialize');
         Route::prefix('{cartId}')->group(function () {
 
-            Route::get('/', [ApiController::class => 'show'])->name('processton-cart.api.show');
+            Route::get('/', [ApiController::class , 'show'])->name('processton-cart.api.show');
 
             Route::prefix('/items')->group(function () {
-                Route::get('/search', [ApiController::class => 'searchItems'])->name('processton-cart.api.items.search');
+                Route::get('/search', [ApiController::class , 'searchItems'])->name('processton-cart.api.items.search');
             });
 
-            Route::post('/add', [ApiController::class => 'addItem'])->name('processton-cart.api.items.add');
-            Route::post('/remove', [ApiController::class => 'removeItem'])->name('processton-cart.api.items.remove');
+            Route::post('/add', [ApiController::class , 'addItem'])->name('processton-cart.api.items.add');
+            Route::post('/remove', [ApiController::class , 'removeItem'])->name('processton-cart.api.items.remove');
         
         });
     });
