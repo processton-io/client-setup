5a3113a532007da1ed7f5980cd1f27ccf53f39d3
Ahmad Faryab Kokab <68786248+afaryab@users.noreply.github.com>
2025-06-04 22:31:39 +0000

    subscribe

--- packages/processton/subscription/routes/web.php
+++ packages/processton/subscription/routes/web.php
@@ -6,15 +6,15 @@ use Processton\Subscription\Controllers\WebController;
 
 Route::middleware([
     'web'
-])->group(function () {
+])->prefix('s/{profile}')->group(function () {
 
-    Route::any('c/{profile}/subscriptions', [WebController::class , 'index'])->name('processton-subscribe.index');
+    Route::any('subscriptions', [WebController::class , 'index'])->name('processton-subscribe.index');
 
-    Route::any('c/{profile}/subscribe/{planId}', [WebController::class , 'subscribe'])->name('processton-subscribe.subscribe');
+    Route::any('subscribe/{planId}', [WebController::class , 'subscribe'])->name('processton-subscribe.subscribe');
 
-    Route::any('c/{profile}/subscription/{subscriptionId}', [WebController::class , 'show'])->name('processton-subscribe.show');
+    Route::any('subscription/{subscriptionId}', [WebController::class , 'show'])->name('processton-subscribe.show');
 
-    Route::any('c/{profile}/subscription/{subscriptionId}/cancel', [WebController::class , 'cancel'])->name('processton-subscribe.cancel');
+    Route::any('subscription/{subscriptionId}/cancel', [WebController::class , 'cancel'])->name('processton-subscribe.cancel');
 
-    Route::any('c/{profile}/subscription/{subscriptionId}/renew', [WebController::class , 'update'])->name('processton-subscribe.renew');
+    Route::any('subscription/{subscriptionId}/renew', [WebController::class , 'update'])->name('processton-subscribe.renew');
 });
